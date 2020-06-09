1d5043928812de4454a755110f1c4edfe3de67d3
unxjj <66663209+unxjj@users.noreply.github.com>
2020-06-09 15:05:32 +0800

    彩票模拟器第一次更新

---
+++ src/main/java/com/xuejj/cpmnq/CpmnqApplication.java
@@ -0,0 +1,13 @@
+package com.xuejj.cpmnq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmnqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmnqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xuejj/cpmnq/CpmnqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuejj.cpmnq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpmnqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
